117ca281f9fc89055bb2ee3c4db8ebc11993f680
Mica Oz <kahulaoz@gmail.coml>
2022-04-12 00:25:34 -0600

    first attempt

---
+++ binarySearch.js
@@ -0,0 +1,14 @@
+//first attempt
+//works but not optimized
+//what is the big O here?
+// var search = function (nums, target) {
+//   let counter = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === target) {
+//       return counter;
+//     }
+//     counter++;
+//   }
+//   return -1;
+// };
